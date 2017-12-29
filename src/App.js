8ae8199fa1fe96d51c945d22b55c6624f0fae102
Joel Scalera <joel.scalera@gmail.com>
2017-12-29 10:46:42 -0700

    first react commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Route, Link } from 'react-router-dom';
+import Landing from './components/Landing';
+import Library from './components/Library';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <h1>Bloc Jams</h1>
+        <nav>
+             <Link to='/'>Landing</Link>
+             <Link to='/library'>Library</Link>
+        </nav>
+        </header>
+      <main>
+        <Route exact path="/" component={Landing} />
+        <Route path="/library" component={Library} />
+      </main>
+      </div>
+    );
+  }
+}
+
+export default App;
